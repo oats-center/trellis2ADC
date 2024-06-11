ab65b9ca540d78a360ba0c01c1b3b7f5b89aaf5b
Aaron Ault <aultac@gmail.com>
2024-06-10 20:03:41 -0400

    Deploying without docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 echo "Starting entrypoint"
 
+echo "ls puppeteer cache:"
+ls -Ral /home/pptruser/.cache/puppeteer
+
 echo "-------- Running yarn"
 yarn
 echo "-------- Running yarn build"
